743888098c943e1ee8755a17bcfb84aac3032120
rajesh5sep-code <rajesh.code@outlook.com>
2022-09-08 00:59:38 +0000

    Update App.js and index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-
+import Context from "./context";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+    <Context.Provider value={"Rajesh"}>
     <App />
+    </Context.Provider>
   </StrictMode>
 );
